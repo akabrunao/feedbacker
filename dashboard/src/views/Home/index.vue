c89d511fbc8d2d9397343be765eb812829b11682
Bruno Rocha <brunoorochaa@gmail.com>
2021-01-22 17:39:06 -0300

    home page

--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,13 +1,45 @@
 <template>
-  <h1 class="text-brand-main font-black">Aqui é a home</h1>
+  <div>
+    <custom-header />
+    <contact
+      @create-account="handleAccountCreate"
+      @login="handleLogin"
+    />
+    <div class="flex justify-center py-10 bg-brand-gray">
+      <p class="font-medium text-center text-gray-800">feedbacker © 2021</p>
+    </div>
+</div>
 </template>
 
 <script>
-export default {
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from './CustomHeader'
+import Contact from './Contact'
 
+export default {
+  components: { CustomHeader, Contact },
+  setup () {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+    function handleLogin () {
+
+    }
+
+    function handleAccountCreate () {
+
+    }
+
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
