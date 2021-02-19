1ce1bd549e84f940ee1c9869a332ec2b2a565ce2
Bruno Rocha <brunoorochaa@gmail.com>
2021-02-19 18:04:52 -0300

    base widget

---
+++ widget/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <widget/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Widget from './views/widget/index.vue'
+
+export default defineComponent({
+  components: { Widget }
+})
+</script>
